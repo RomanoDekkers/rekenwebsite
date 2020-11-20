16bcc9743aa78e0b4fe9c29fb80299a35fbb537e
Daan de Klein <0085940@rocdeleijgraaf.nl>
2020-11-20 11:17:08 +0100

    begin gemaakt

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-"teskt"
-"bababoeey"
+<html>
+<head>
+    <title>desplinter rekenwebsite</title>
+</head>
+<body>
+    <h1>"logo"   Groep 4   Groep 5   Groep 6   Uitleg/video's</h1>
+    <hr /> 
+</body>
+</html>
