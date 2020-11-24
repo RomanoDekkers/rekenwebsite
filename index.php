5b5edf23392d0d5617dd6b31860e294d62440449
Romano Dekkers <0085420@rocdeleijgraaf.nl>
2020-11-24 13:32:02 +0100

    update

--- index.php
+++ index.php
@@ -8,12 +8,29 @@
 <nav>
     <ul>
         <li><img src="logo.jpg" alt="logo" width="80px" height="80px" id="logo"></li>
-        <li><a href="#">Home</a></li>
+        <li><a href="http://localhost/desplinter/">Home</a></li>
         <li><a href="#">Groep 4</a></li>
         <li><a href="#">Groep 5</a></li>
         <li><a href="#">Groep 6</a></li>
+        <li><a href="#">uitleg en video's</a></li>
     </ul>
 </nav>
+<div class="slider-frame">
+    <div class="slide-images">
+        <div class="img-container">
+            <img src="foto_1.jpg" height = "300" width = "500">
+        </div>
+        <div class="img-container">
+            <img src="foto_2.jpg" height = "300" width = "500">
+        </div>
+        <div class="img-container">
+            <img src="foto_3.jpg" height = "300" width = "500">
+        </div>
+        <div class="img-container">
+            <img src="foto_4.jpg" height = "300" width = "500">
+        </div>
+    </div>
+</div>
 </body>
 </html>
 
