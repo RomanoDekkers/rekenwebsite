f4f379fdf951342e316708b00fe108f4534a32c2
Romano Dekkers <0085420@rocdeleijgraaf.nl>
2020-11-25 14:06:39 +0100

    slideshow

--- index.php
+++ index.php
@@ -8,26 +8,27 @@
 <nav>
     <ul>
         <li><img src="logo.jpg" alt="logo" width="80px" height="80px" id="logo"></li>
-        <li><a href="http://localhost/desplinter/">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="#">Groep 4</a></li>
         <li><a href="#">Groep 5</a></li>
         <li><a href="#">Groep 6</a></li>
         <li><a href="#">uitleg en video's</a></li>
+        <li><a href="#" id="contact">contact opnemen</a></li>
     </ul>
 </nav>
 <div class="slider-frame">
     <div class="slide-images">
         <div class="img-container">
-            <img src="foto_1.jpg" height = "300" width = "500">
+            <img src="foto_1.jpg" height = "250" width = "400">
         </div>
         <div class="img-container">
-            <img src="foto_2.jpg" height = "300" width = "500">
+            <img src="foto_2.jpg" height = "250" width = "400">
         </div>
         <div class="img-container">
-            <img src="foto_3.jpg" height = "300" width = "500">
+            <img src="foto_3.jpg" height = "250" width = "400">
         </div>
         <div class="img-container">
-            <img src="foto_4.jpg" height = "300" width = "500">
+            <img src="foto_4.jpg" height = "250" width = "400">
         </div>
     </div>
 </div>
