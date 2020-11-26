8ca798123304bcfe968c1ccc8f6f4801a81c35fa
Daan de Klein <0085940@rocdeleijgraaf.nl>
2020-11-26 11:36:14 +0100

    drop down menu geadd

--- index.php
+++ index.php
@@ -12,11 +12,21 @@
         <li><a href="#">Groep 4</a></li>
         <li><a href="#">Groep 5</a></li>
         <li><a href="#">Groep 6</a></li>
-        <li><a href="#">uitleg en video's</a></li>
+        <li><a href="#">uitleg en video's</a>
+            <ul>
+                <li><a href="">video 1</a></li>
+                <li><a href="">video 2</a></li>
+                <li><a href="">video 3</a></li>
+                <li><a href="">video 4</a></li>
+            </ul>
+        </li>
         <li><a href="#" id="contact">contact opnemen</a></li>
     </ul>
 </nav>
-
+<br />
+<br />
+<h1>De Splinter</h1>
+<p>Hier komt info over de school</p>
 </body>
 </html>
 
