a3863c6dd05390064df1dd26304bfe654aa574ef
Romano Dekkers <0085420@rocdeleijgraaf.nl>
2020-12-03 10:43:22 +0100

    nav bar toevoegen

---
+++ groep6.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>contactformulier</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<nav>
+    <ul>
+        <li><img src="logo.jpg" alt="logo" width="80px" height="80px" id="logo"></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="groep4.php">Groep 4</a></li>
+        <li><a href="groep5.php">Groep 5</a></li>
+        <li><a href="groep6.php">Groep 6</a></li>
+        <li><a href="#">hulpvideo's</a>
+            <ul>
+                <li><a href="https://www.youtube.com/watch?v=1gmUWGt2KRg" target="_blank">videouitleg plus</a></li>
+                <li><a href="https://www.youtube.com/watch?v=adEkIa42Y2k&t=132s" target="_blank">videouitleg min</a></li>
+                <li><a href="https://www.youtube.com/watch?v=d79H5i5SUd8" target="_blank">videouitleg keer</a></li>
+                <li><a href="https://www.youtube.com/watch?v=4PyzMWQ-7B8" target="_blank">videouitleg delen</a></li>
+            </ul>
+        </li>
+        <li><a href="contact.php" id="contact">contact opnemen</a></li>
+    </ul>
+</nav>
+</body>
+</html>
