586b8f3b67fb5e0f772410b51d43cd52dd0a2791
Daan de Klein <0085940@rocdeleijgraaf.nl>
2020-12-03 10:36:40 +0100

    alle tabs toe gevoegd

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
     <ul>
         <li><img src="logo.jpg" alt="logo" width="80px" height="80px" id="logo"></li>
         <li><a href="index.php">Home</a></li>
-        <li><a href="#">Groep 4</a></li>
-        <li><a href="#">Groep 5</a></li>
-        <li><a href="#">Groep 6</a></li>
+        <li><a href="groep4.php">Groep 4</a></li>
+        <li><a href="groep5.php">Groep 5</a></li>
+        <li><a href="groep6.php">Groep 6</a></li>
         <li><a href="#">hulpvideo's</a>
             <ul>
                 <li><a href="https://www.youtube.com/watch?v=1gmUWGt2KRg" target="_blank">videouitleg plus</a></li>
@@ -20,7 +20,7 @@
                 <li><a href="https://www.youtube.com/watch?v=4PyzMWQ-7B8" target="_blank">videouitleg delen</a></li>
             </ul>
         </li>
-        <li><a href="#" id="contact">contact opnemen</a></li>
+        <li><a href="contact.php" id="contact">contact opnemen</a></li>
     </ul>
 </nav>
 <br />
@@ -28,7 +28,7 @@
 <h1>De Splinter</h1>
 <br/>
 <h2>Openingstijden:</h2>
-<p>maandag tot en met dinsdag van 08:00 tot en met 17:30</p>
+<p>maandag tot en met vrijdag van 08:00 tot en met 17:30</p>
 <br/>
 <h2>Vakantierooster:</h2>
 <br/>
