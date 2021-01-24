50b951f9fbc852b0a70281325a6be8fba1b0e6c8
Romano Dekkers <0085420@rocdeleijgraaf.nl>
2021-01-25 00:28:47 +0100

    contactformulier

--- contact.php
+++ contact.php
@@ -22,6 +22,33 @@
         <li><a href="contact.php" id="contact">contact opnemen</a></li>
     </ul>
 </nav>
+	<form name="contactformulier" action="" method="post">
+		<h1 class="contact">contactformulier</h1>
+		<h3 class="contact">uw naam:<h3>
+		<input type="text" name="username" size="15" class="vul"/>
+		<h3 class="contact">in welke klas zit u?</h3>
+		</br>
+		<p class="contact">Groep 4</p>
+		<input type="checkbox" name="Groep4" class="vul"/> 
+		</br>
+		<p class="contact">Groep 5</p>
+		<input type="checkbox" name="Groep5" class="vul"/> 
+		</br>
+		<p class="contact">Groep 6</p>
+		<input type="checkbox" name="Groep6" class="vul"/> 
+		</br>
+		<h3 class="contact">beschrijf uw probleem</h3>
+		<br/>
+		<textarea name="comments" cols="20" rows="3" class="vul"></textarea>
+		<br/>
+		<input type="submit" name="submit" value="Versturen" class="vul"/>
+	</form>
 
 </body>
 </html>
+<?php
+if(isset($_POST['submit']))
+{
+echo "test";
+}
+?>
